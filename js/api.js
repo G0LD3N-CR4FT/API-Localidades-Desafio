90f1c90163adb3d06a803ff9c5d525c981b50605
G0LD3N-CR4FT <eduardomaster.tellis@gmail.com>
2022-09-21 08:12:37 -0300

    Finalização Desafio

---
+++ js/api.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+$('#aparecer').hide()
+
+
+    $.ajax({
+        type: 'GET',
+        dataType: 'JSON',
+        url: "https://servicodados.ibge.gov.br/api/v1/localidades/estados",
+        success: function(dados){
+            $.each(dados, function(indexInArray, valueOfElement){
+            var ufs = "<option>"+valueOfElement.sigla+"</option>"
+            $("#estado").append(ufs)
+            });
+        }
+    });
+$("#estado").change (function(e){
+    e.preventDefault();
+    $("#cidade").empty();
+    var uf = $("#estado").val();
+    $('#aparecer').show()
+
+$.ajax({
+    type: 'GET',
+    dataType: 'JSON',
+    url: "https://servicodados.ibge.gov.br/api/v1/localidades/estados/"+uf+"/municipios",
+    data: {orderBy: "nome"},
+    success: function(dados){
+        $.each(dados, function(indexInArray, valueOfElement){
+        var cidade = "<option>"+valueOfElement.nome+"</option>"
+        $("#cidade").append(cidade)
+        });
+    }
+});
+});
+});
